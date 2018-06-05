21487936a6a4b65aba97f958b9183cddd47b9192
Moffat Ombasa <moffatombasa@yahoo.com>
2018-06-05 17:35:18 +0300

    code for the index.html and css added

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//business logic
+var numbersToBePingponged = [];
+
+function pingPong (number){
+    for (var index = 1; index <= number; index+=1){
+        if(index % 15 === 0){
+            numbersToBePingponged.push("pingpong");
+        }
+        else if(index % 5 === 0){
+            numbersToBePingponged.push("pong");
+        }
+        else if(index % 3 === 0){
+            numbersToBePingponged.push("ping");
+        }
+        else{
+            numbersToBePingponged.push(index);
+        }
+    }
+}
